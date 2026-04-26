cf2027b9e95485a231fdb53d4633dd87eadb2522
Creeper_musume <141794440+EndeeerMan@users.noreply.github.com>
2026-04-26 12:13:26 +0900

    Refactor input handling to use string array

--- 符文净化/test.c
+++ 符文净化/test.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
+char str[10005];
+
 int main(){
-    int size = 0;
-    int buf = getchar();
-    while(buf != -1 && buf != '\0' && buf != '\n'){
-        if(buf == '6') size++;
-        buf = getchar();
-    }
-    for(int i=1;i<=size;i++){
-        putchar('6');
+    scanf("%s",str);
+
+    int i = 0;
+    int temp = str[i++] - '0';
+
+    while(temp >= 0 && temp <= 9){
+        if(temp == 6){
+            printf("6");
+        }
+        temp = str[i++] - '0';
     }
 }
